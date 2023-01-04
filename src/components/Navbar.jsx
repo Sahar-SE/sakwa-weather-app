4e3907765672719f622b0201aaf788cea8c834f4
Sahar Saba Amiri <www.saharsaba.amiri123@gmail.com>
2023-01-04 22:36:24 +0430

    Add new functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 //import { FaMicrophone } from 'react-icons/fa';
 //import { AiOutlineSetting } from 'react-icons/ai';
-import { IoChevronBackOutline } from 'react-icons/io5';
+// import { IoChevronBackOutline } from 'react-icons/io5';
 
 const Navbar = () => (
   <header className="padding">
     <nav>
       <ul>
         <li className="navLinks">
-          <Link exact="true" to="/">
-            <IoChevronBackOutline className="backIcon" />
-          </Link>
+//           <Link exact="true" to="/">
+//             <IoChevronBackOutline className="backIcon" />
+//           </Link>
           <Link exact="true" to="/" className="navlink">
             Back
           </Link>
